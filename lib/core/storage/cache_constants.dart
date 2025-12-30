ce7800ed7e8cf9e802b141341fc9158ba2c02fc4
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-12-30 21:02:38 +0200

    Initialize SharedPreferences in main and implement CacheHelper for data storage management

---
+++ lib/core/storage/cache_constants.dart
@@ -0,0 +1,4 @@
+class AppConstants {
+  // Theme
+  static const String themeKey = 'theme_mode';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router/app_router.dart';
+import 'core/storage/cache_helper.dart';
 import 'recipe_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
   runApp(ProviderScope(child: RecipeApp(appRouter: AppRouter())));
 }
